864add345b8aa438f610f847385174b31128a03b
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-08-25 10:02:49 +0000

    [BUGFIX] Remove calls to deprecated methods in TYPO3 4.6
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mytypo3/trunk@51348 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,10 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-if (t3lib_div::int_from_ver(TYPO3_version) < 4007000) {
+$version = class_exists('t3lib_utility_VersionNumber')
+		? t3lib_utility_VersionNumber::convertVersionNumberToInteger(TYPO3_version)
+		: t3lib_div::int_from_ver(TYPO3_version);
+if ($version < 4006000) {
 	$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['typo3/mod/help/about/index.php'] = t3lib_extMgm::extPath($_EXTKEY) . 'xclass/class.tx_mytypo3_xclass_about.php';
 }
 
